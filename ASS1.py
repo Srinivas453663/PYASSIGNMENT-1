9678b48dc62a2513d8b04033d2e6bb0b5c2d869a
Srinivas <dashsrinivas411@gmail.com>
2025-05-11 12:53:07 +0530

    Add files via upload

---
+++ ASS1.py
@@ -0,0 +1,17 @@
+a=int(input("Enter the first No.:"))
+b=int(input("Enter the second No.:"))
+
+add=a+b
+print("Addition :" , add)
+sub=a-b
+print("Subtraction :" , sub)
+mul=a*b
+print("Multiplication :", mul)
+div=a/b
+print("Division :" , div)
+
+
+
+
+
+
---
+++ ASS2.py
@@ -0,0 +1,4 @@
+a=input("Enter your first name:")
+b=input("Enter your second name:")
+
+print(f"Hello,{a} {b}! Welcome to the python program.")
